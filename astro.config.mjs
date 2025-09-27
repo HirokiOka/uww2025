115b5eef03a0cc9b7f09409f96239b3725c00f83
hiroka <mementoka@gmail.com>
2025-09-27 12:50:18 +0900

    本番用にパスを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://https://tt-lab.jp/uww2025/",
+    base: "uww2025",
+});
