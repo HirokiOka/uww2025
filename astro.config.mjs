3b6877b0a47bb9d22df9daee9ee2e56776cbbef9
hiroka <mementoka@gmail.com>
2025-12-07 13:31:11 +0900

    プログラムページの実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://https://tt-lab.jp/uww2025/",
-    base: "uww2025",
+  site: "https://https://tt-lab.jp/uww2025/",
+  base: "uww2025",
 });
